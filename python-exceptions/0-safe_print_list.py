9b9a52a8e0eaad085586f45f3f7791f6466dcc6d
cinna03 <c.mwiza1@alustudent.com>
2024-08-07 00:23:31 -0700

    exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    try:
-        for item in range(x):
-            print("()".format(my_list[item]), end='')
-            num += 1
-   except IndexError:
-       pass
-   print()
-   return num
+    func = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            func += 1
+        except IndexError:
+            break
+    print("")
+    return (func)
 
        
