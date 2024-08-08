a1080d6f43ee2ffc48d1cd28c9f71e132102fbbc
cinna03 <c.mwiza1@alustudent.com>
2024-08-08 03:38:13 -0700

    test drive 05

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""A function to sperate words in python"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
-    if not isinstance(text, str):
+    '''Fucntion to bring the texts'''
+    if type(text) != str:
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)]
+        )
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    print("{}".format(text), end="")
