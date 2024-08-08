3ba24e93ed036cf7ca0e8fb29d5a873cd5b54612
cinna03 <c.mwiza1@alustudent.com>
2024-08-08 03:32:12 -0700

    test drive 03

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+'''A function that divs a martix with a number'''
 
 
-def say_my_name(first_name, last_name=""):
-    """Print a name.
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+def matrix_divided(matrix, div):
+    """"A function that divs a martix with a number"""
+    if not isinstance(matrix, (list,)):
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
+    for row in matrix:
+        if type(row) != list:
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError("matrix must be a matrix"
+                                " (list of lists) of integers/floats")
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    mat_new = []
+    for i in range(len(matrix)):
+        mat_new.append(list())
+        for j in range(len(matrix[i])):
+            mat_new[i].append(round(matrix[i][j] / div, 2))
+    return mat_new
