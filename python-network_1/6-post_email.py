af24d4693870906b3174612f5d9a4b03bb824296
cinna03 <c.mwiza1@alustudent.com>
2024-08-09 14:39:04 -0700

    NETWORK 101

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""
-A Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email,
-and displays the body of the response
-"""
+"""take a url and display the values"""
+
+
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print(r.text)
+    """send the email"""
+    url = sys.argv[1]
+    email = sys.argv[2]
+    context = {
+        "email": email
+    }
+    response = requests.post(url, data=context)
+    print("{}".format(response.text))
