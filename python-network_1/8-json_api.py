1fc6b6ab0c9dc00a29bd85526c3af3fac29e9773
cinna03 <c.mwiza1@alustudent.com>
2024-08-09 14:41:46 -0700

    NETWORK 101

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""
-A Python script that sends a POST request to the URL and
-to an URL with the letter as a parameter
-"""
+"""send letter"""
 import requests
 import sys
 
-
-if __name__ == "__main__":
-    data = {'q': ""}
-
+if __name__ == '__main__':
     try:
-        data['q'] = sys.argv[1]
-    except:
-        pass
-
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
-
+        lett = sys.argv[1]
+    except IndexError:
+        lett = ""
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user",
+        data={"q": lett}
+    )
     try:
-        json_o = r.json()
-        if not json_o:
-            print("No result")
-        else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+        json_response = response.json()
+        if response.headers.get("Content-Type") == 'application/json':
+            if len(json_response) > 0:
+                print("[{}] {}".format(
+                    json_response["id"],
+                    json_response["name"])
+                )
+            else:
+                print("No result")
     except:
         print("Not a valid JSON")
