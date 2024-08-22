068e84c05bc11654c9e6b6b7854fdc62159eb114
cinna03 <c.mwiza1@alustudent.com>
2024-08-22 00:55:25 -0700

    test drive 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-'''A function that divs a martix with a number'''
+"""A function that prints the sqaures"""
 
 
-def matrix_divided(matrix, div):
-    """"A function that divs a martix with a number"""
-    if not isinstance(matrix, (list,)):
-        raise TypeError("matrix must be a matrix "
-                        "(list of lists) of integers/floats")
-    for row in matrix:
-        if type(row) != list:
-            raise TypeError("matrix must be a matrix "
-                            "(list of lists) of integers/floats")
-        for item in row:
-            if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix"
-                                " (list of lists) of integers/floats")
-    row_size = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_size:
-            raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+def print_square(size):
+    '''FUnction to print the squares
+    Args:
+        size : must be an integer
+    Returns:
+        prints the square
+    '''
+    square_char = "#"
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
 
-    mat_new = []
-    for i in range(len(matrix)):
-        mat_new.append(list())
-        for j in range(len(matrix[i])):
-            mat_new[i].append(round(matrix[i][j] / div, 2))
-    return mat_new
+    for i in range(size):
+        for j in range(size):
+            print(square_char, end="")
+        print()
