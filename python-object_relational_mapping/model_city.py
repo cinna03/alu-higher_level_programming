fc8ad9447b59c354e9c960b351db53ec9f9e408a
cinna03 <c.mwiza1@alustudent.com>
2024-08-22 01:45:45 -0700

    random

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" class definition of a State and an instance"""
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+"""class City inherits from Base"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
-class State(Base):
-    """Tables states"""
-    __tablename__ = 'states'
+class City(Base):
+    """Class City"""
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
