22c173be91a417ab3bfe1f1cfbc18822ea07252a
cinna03 <c.mwiza1@alustudent.com>
2024-08-21 23:47:58 -0700

    data structures 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
-st_integer(my_list=[]):
-    if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
