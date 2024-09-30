964a452f24b8fe1fc9a514b63de972414ea9aa5b
cinna03 <c.mwiza1@alustudent.com>
2024-09-30 07:28:21 +0000

    scraping 102

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+t request = require('request');
+
+if (process.argv.length > 2) {
+  const url = process.argv[2];
+  request.get(url, (error, response) => {
+    if (error) {
+      console.error(`An error occurred while making the request: ${error}`);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
+  });
+} else {
+  console.log('provide the URL as an argument.');
+}
